f04c9bc81ebb3eaf081b53f7f4f1551533e2c09b
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-03-23 07:52:40 -0300

    07 - Cadastrar Categoria API Laravel

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,10 +8,24 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
     public function index(Request $request, Category $category)
     {
         $categories = $category->getResults($request->name);
 
         return response()->json($categories, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $catgory = $this->category->create($request->all());
+
+        return response()->json($catgory, 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 //$this->get('categories', 'Api\CategoryController@index');
 Route::get('/categories', 'Api\CategoryController@index');
+Route::post('/categories', 'Api\CategoryController@store');
 
 
 
