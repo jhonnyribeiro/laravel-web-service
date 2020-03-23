59df27d2abcd2b077043ea7a2d779a4b46dcb93e
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-03-23 07:46:05 -0300

    06 - Opção de Filtros em Categorias Laravel Web Services

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(Category $category)
+    public function index(Request $request, Category $category)
     {
-        $categories = $category->all();
+        $categories = $category->getResults($request->name);
 
         return response()->json($categories, 200);
     }
