899b13e36e8ffe4bef5ca282a454741e9ba9f61d
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-04-14 19:42:39 -0300

    15 - Factory Produtos API Laravel

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,13 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
 use Faker\Generator as Faker;
 Use App\Models\Product;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->unique()->word,
         'description' => $faker->sentence(),
     ];
 });
+
+
