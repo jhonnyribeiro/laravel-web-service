1e378f3801c1f97b9e613218411b760f161d4ab9
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-04-15 05:59:32 -0300

    20 - Cadastrar Produtos API

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -43,7 +43,9 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = $this->product->create($request->all());
+
+        return response()->json($product, 201);
     }
 
     /**
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'description', 'image'];
+    protected $fillable = ['name', 'description', 'image', 'category_id'];
 
     public function getResults($data, $total)
     {
