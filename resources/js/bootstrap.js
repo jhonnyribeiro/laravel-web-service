929fb08a7214469e4fd45c712369db5d48291f14
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-04-19 23:02:52 -0300

    VueJS 01.05 - Entendo os resources JS do Laravel

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,6 +6,8 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+
+
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -15,6 +17,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
+
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 // import Echo from 'laravel-echo';
 
